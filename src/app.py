a5312d33776c38c3e68ac934a3e5fe2adc817a1e
alevivas <ale_993@hotmail.com>
2024-08-27 09:19:24 -0300

    New Changes add PDF

--- src/app.py
+++ src/app.py
@@ -14,6 +14,12 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 from langchain.chains import VectorDBQA
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+
 
 load_dotenv()
 
@@ -71,41 +77,212 @@ def get_response(user_input):
     
     return response['answer']
 
-# app config
-st.set_page_config(page_title="Chat with websites", page_icon="🤖")
-st.title("Chat with websites")
 
-# sidebar
-with st.sidebar:
-    st.header("Settings")
-    website_url = st.text_input("Website URL")
-
-if website_url is None or website_url == "":
-    st.info("Please enter a website URL")
-
-else:
-    # session state
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = [
-            AIMessage(content="Hello, I am a bot. How can I help you?"),
-        ]
-    if "vector_store" not in st.session_state:
-        st.session_state.vector_store = get_vectorstore_from_url(website_url)    
-
-    # user input
-    user_query = st.chat_input("Type your message here...")
-    if user_query is not None and user_query != "":
-        response = get_response(user_query)
-        st.session_state.chat_history.append(HumanMessage(content=user_query))
-        st.session_state.chat_history.append(AIMessage(content=response))
+def handle_pdf():
+    st.header("Ask your PDF 💬")
+    
+    # upload file
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+    
+    # extract the text
+    if pdf is not None:
+      pdf_reader = PdfReader(pdf)
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
         
-       
+      # split into chunks
+      text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+      )
+      chunks = text_splitter.split_text(text)
+      
+      # create embeddings
+      embeddings = OpenAIEmbeddings()
+      knowledge_base = FAISS.from_texts(chunks, embeddings)
+      
+      # show user input
+      user_question = st.text_input("Ask a question about your PDF:")
+      if user_question:
+        docs = knowledge_base.similarity_search(user_question)
+        
+        llm = ChatOpenAI()
+        chain = load_qa_chain(llm, chain_type="stuff")
+        with get_openai_callback() as cb:
+          response = chain.run(input_documents=docs, question=user_question)
+          print(cb)
+           
+        st.write(response)
+        
+def handle_website():
+        
+    st.title("Chat with websites")
 
-    # conversation
-    for message in st.session_state.chat_history:
-        if isinstance(message, AIMessage):
-            with st.chat_message("AI"):
-                st.write(message.content)
-        elif isinstance(message, HumanMessage):
-            with st.chat_message("Human"):
-                st.write(message.content)
+    # sidebar
+    with st.sidebar:
+        website_url = st.text_input("Website URL")
+
+    if website_url is None or website_url == "":
+        st.info("Please enter a website URL")
+
+    else:
+        # session state
+        if "chat_history" not in st.session_state:
+            st.session_state.chat_history = [
+                AIMessage(content="Hello, I am a bot. How can I help you?"),
+            ]
+        if "vector_store" not in st.session_state:
+            st.session_state.vector_store = get_vectorstore_from_url(website_url)    
+
+        # user input
+        user_query = st.chat_input("Type your message here...")
+        if user_query is not None and user_query != "":
+            response = get_response(user_query)
+            st.session_state.chat_history.append(HumanMessage(content=user_query))
+            st.session_state.chat_history.append(AIMessage(content=response))
+            
+        
+
+        # conversation
+        for message in st.session_state.chat_history:
+            if isinstance(message, AIMessage):
+                with st.chat_message("AI"):
+                    st.write(message.content)
+            elif isinstance(message, HumanMessage):
+                with st.chat_message("Human"):
+                    st.write(message.content)
+
+
+# app config
+st.set_page_config(page_title="Algorilabs Playground", page_icon="🤖")
+
+# CSS for styling the sidebar and images
+st.markdown(
+    """
+    <style>
+    /* Change the background color of the sidebar */
+    [data-testid="stSidebar"] {
+        background-color: #010e17;
+    }
+
+    [data-testid="stHeader"] {
+             background-color: #1a2a38;
+    }
+    
+   [data-testid="stBottom"] > div {
+    background-color: #1a2a38;
+}
+    
+    .main {
+        background-color: #1a2a38;
+        color: white;
+    }
+
+      /* Change the color of all text elements to white */
+    h1, h2, h3, h4, h5, h6, p, div, span {
+        color: white;
+    }
+
+    /* Style the input boxes */
+    input {
+        background-color: #010e17;
+        color: white;
+        border: 1px solid #e9c46a;
+        border-radius: 5px;
+    }
+
+    /* Style the text input box */
+    input::placeholder {
+        color: #e9c46a;
+    }
+
+    /* Style the "Settings" header */
+    .stTextInput > div > label {
+        color: white;
+        font-weight: bold;
+        font-size: 18px;
+    }
+
+    .sidebar-title {
+        font-size: 28px;
+        font-weight: bold;
+        color: white;
+        margin-bottom: 30px;
+    }
+
+    /* Style for the option buttons */
+    .sidebar-option {
+        font-size: 22px;
+        font-weight: bold;
+        color: #ffffff;
+        padding: 10px;
+        border-radius: 5px;
+        margin-bottom: 10px;
+        background-color: #46db9a;
+        text-align: center;
+    }
+    .sidebar-option:hover {
+        background-color: #f4a261;
+        cursor: pointer;
+    }
+    .sidebar-option-selected {
+        background-color: #f4a261;
+        color: white;
+    }
+    .stButton>button {
+        background-color: #46db9a;
+        color: white;
+        font-size: 22px !important;
+        font-weight: bold;
+        border-radius: 5px;
+        margin-bottom: 10px;
+        width: 100%;
+    }
+    .stButton>button:hover {
+        background-color: #f4a261;
+        color: white;
+    }
+      /* Estilo personalizado para el input de URL */
+    .stTextInput label {
+        font-size: 20px;
+        color: #FFFFFF; /* Color blanco */
+    }
+    .stTextInput div {
+        font-size: 18px;
+        color: #FFFFFF; /* Color blanco */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Function to select an option and simulate radio button logic
+def select_option(option):
+    st.session_state.selected_option = option
+
+# Sidebar with options
+st.sidebar.image("assets/AlgoriLabs.jpeg", use_column_width=True)
+st.sidebar.markdown('<div class="sidebar-title">Welcome to Algorilabs Playground</div>', unsafe_allow_html=True)
+
+# Check the current selection
+if "selected_option" not in st.session_state:
+    st.session_state.selected_option = "Chat with a Website"
+
+# Button 1: Chat with a Website
+if st.sidebar.button("Chat with a Website", key="btn1"):
+    select_option("Chat with a Website")
+
+# Button 2: Chat with a PDF
+if st.sidebar.button("Chat with a PDF", key="btn2"):
+    select_option("Chat with a PDF")
+
+# Displaying selected option in the main area
+if st.session_state.selected_option == "Chat with a Website":
+    st.image("assets/AIWebSite.png", width=300)
+    handle_website()
+elif st.session_state.selected_option == "Chat with a PDF":
+    st.image("assets/AIPdf.png",  width=300)
+    handle_pdf()
